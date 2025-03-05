6ad94e7a310aca4184e0dbb1e3877b00e279eede
gwoessner <guillaume@woessner.ovh>
2025-03-05 18:30:08 +0000

    Deploying to gh-pages from @ gwoessner/gwoessner.github.io@532503ee1e619ce748d116f3763f1298d04d9796 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,35 +492,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%75%69%6C%6C%61%75%6D%65@%77%6F%65%73%73%6E%65%72.%6F%76%68", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/guillaume-woessner-700449227", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-researchgate',
+        title: 'ResearchGate',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.researchgate.net/profile/Guillaume-Woessner/", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/Charles_WG3", "_blank");
         },
       },{
       id: 'light-theme',
