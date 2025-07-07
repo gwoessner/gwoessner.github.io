50cf61156662c21190c8671e7f0dfe0d0ec87e09
gwoessner <guillaume@woessner.ovh>
2025-07-07 13:14:56 +0000

    Deploying to gh-pages from @ gwoessner/gwoessner.github.io@ec424439bcaa3a441e4fd68311450865c9d6b10c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -470,13 +470,6 @@ ninja.data = [{
           window.open("https://www.researchgate.net/profile/Guillaume-Woessner/", "_blank");
         },
       },{
-        id: 'social-x',
-        title: 'X',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://twitter.com/Charles_WG3", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
