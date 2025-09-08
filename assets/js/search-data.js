572864ea0e05c02266bdca99c31b229a6f690f86
gwoessner <guillaume@woessner.ovh>
2025-09-08 20:26:22 +0000

    Deploying to gh-pages from @ gwoessner/gwoessner.github.io@94207270fb6161df6f249dc1bc9c6c0c71178d14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-blog",
           title: "Blog",
-          description: "Là où je mets quelques articles écrits pour le compte de l&#39;ARPADIS.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
