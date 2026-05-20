066ed07c9e90ad74f2cf24a0d8fb2caa0c209688
gwoessner <guillaume@woessner.ovh>
2026-05-20 20:32:07 +0000

    Deploying to gh-pages from @ gwoessner/gwoessner.github.io@43318225a68f24659eae55f99c2b093a8cf64ea6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -103,17 +103,6 @@ ninja.data = [{
           window.location.href = "/blog/2025/climat/";
         
       },
-    },{id: "post-où-va-l-39-argent-des-travailleurs-en-cours",
-      
-        title: "Où va l&#39;argent des travailleurs ? (EN COURS)",
-      
-      description: "Les éclairages de l&#39;économie sur comment mieux répartir le travail.",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/recap-economie/";
-        
-      },
     },{id: "post-qu-39-est-ce-que-la-science",
       
         title: "Qu&#39;est-ce que la science ?",
